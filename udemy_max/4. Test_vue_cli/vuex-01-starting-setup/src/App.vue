1b7202f1174b776bcd35746670abcff78f11a838
Ari Umboro Seno <ariumboroseno@Umboros-MacBook-Air.local>
2021-11-22 07:08:20 +0700

    VUEX

--- udemy_max/4. Test_vue_cli/vuex-01-starting-setup/src/App.vue
+++ udemy_max/4. Test_vue_cli/vuex-01-starting-setup/src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+      <TheCounter/>
+      <CounterWithGetters/>
+      <button @click="addOne">Add 1</button>
+      <button @click="addWithParam">Add With Param</button>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import CounterWithGetters from './components/CounterWithGetters.vue';
+
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    CounterWithGetters
   },
+  methods: {
+    addOne() {
+      this.$store.commit("increment");
+    },
+    addWithParam() {
+      let param = {
+        value : 10
+      };
+      this.$store.commit("incrementWithValue", param);
+    }
+  }
 };
 </script>
 
---
+++ udemy_max/4. Test_vue_cli/vuex-01-starting-setup/src/components/CounterWithGetters.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>{{ counter }}</h3>
+        <p>We Do More ...</p>
+    </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        counter() {
+            // return this.$store.getters.finalCounter;
+            return this.$store.getters.normalizeCounter;
+        }
+    },
+}
+</script>
